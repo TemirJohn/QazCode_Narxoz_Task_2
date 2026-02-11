1f06d1a527c4f6ac705485eee374413499859885
TemirJohn <timur.dzhuldasov@outlook.com>
2026-02-11 10:21:23 +0500

    finish task 4

--- src/main/java/org/Temirjohn/Task_4/Task_4.java
+++ src/main/java/org/Temirjohn/Task_4/Task_4.java
@@ -1,23 +1,47 @@
 package org.Temirjohn.Task_4;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.nio.charset.StandardCharsets;
 
 public class Task_4 {
     public static void main(String[] args) throws Exception {
-        ServerSocket serverSocket = new ServerSocket(8081);
-        Socket input = serverSocket.accept();
-        Scanner in = new Scanner(input.getInputStream());
-        while (in.hasNext()) {
-            System.out.println(in.nextLine());
+        try (ServerSocket serverSocket = new ServerSocket(8081)) {
+            while (true) {
+                Socket client = serverSocket.accept();
+
+                new Thread(() -> Client(client)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        in.close();
-        input.close();
-        serverSocket.close();
     }
 
-//    private static void Client(Socket socket) {
-//        try (socket; IN)
-//    }
+    private static void Client(Socket socket) {
+        try (socket;
+             InputStream in = socket.getInputStream();
+             OutputStream out = socket.getOutputStream()) {
+            byte[] buffer = new byte[1024];
+            int read = in.read(buffer);
+            String request = new String(buffer, 0, read);
+            System.out.println("Gets request: " + request);
+
+            String responseBody = "<html><body><h1>Hello from Mergen Temirzhan!</h1></body></html>";
+            String httpResponse = "HTTP/1.1 200 OK\r\n" +
+                    "Content-Type: text/html; charset=utf-8\r\n" +
+                    "Content-Length: " + responseBody.getBytes(StandardCharsets.UTF_8).length + "\r\n" +
+                    "Connection: close\r\n" +
+                    "\r\n" +
+                    responseBody;
+
+            out.write(httpResponse.getBytes(StandardCharsets.UTF_8));
+            out.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
